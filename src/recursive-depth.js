b659443f42f91ca7c7f754fea47ac1eb8d321567
scalette <furca7e@gmail.com>
2022-10-16 01:22:37 +0200

    feat: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  recursDepth(arr){
+    if(arr.some(el => Array.isArray(el))){
+      this.count++
+      this.recursDepth(arr.flat())
+    }
+  }
+  calculateDepth(arr, sec) {
+    if (sec){
+      return 1
+    }
+    const test = this.calculateDepth([1], true)
+    const test2 = this.calculateDepth([1], true)
+    this.count = 1
+    this.recursDepth(arr)
+    return this.count
   }
 }
-
+const inst = new DepthCalculator()
+console.log(inst.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]))
+console.log(inst.calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]))
 module.exports = {
   DepthCalculator
 };
